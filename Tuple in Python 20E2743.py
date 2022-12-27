b9b81d141339008e0219d5a7ea796075434c9376
YUVASHREE1 <121479511+YUVASHREE1@users.noreply.github.com>
2022-12-27 12:57:20 +0530

    Add files via upload

---
+++ Tuple in Python 20E2743.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Tuple
+a = ("apple", "banana", "cherry")
+print(a)
+
+
+# In[2]:
+
+
+#Allow Duplicates
+t = ("apple", "banana", "cherry", "apple", "cherry")
+print(t)
+
+
+# In[3]:
+
+
+#Tuple Length
+c = ("apple", "banana", "cherry")
+print(len(c))
+
+
+# In[7]:
+
+
+#Create Tuple With One Item
+a = ("apple",)
+print(type(a))
+
+
+# In[6]:
+
+
+a = ("apple")
+print(type(a))
+
+
+# In[8]:
+
+
+#type()
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+
+# In[9]:
+
+
+#The tuple() Constructor
+tuple1 = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(tuple1)
+
+
+# In[10]:
+
+
+#Access Tuple Items
+W = ("apple", "banana", "cherry")
+print(W[1])
+
+
+# In[11]:
+
+
+#Negative Indexing
+A = ("apple", "banana", "cherry")
+print(A[-1])
+
+
+# In[12]:
+
+
+#Range of Indexes
+t = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(t[2:5])
+
+
+# In[ ]:
+
+
+
+
